28feed4161f3ba8bc569e623cfa87f84b30935c4
ZiyanGZiyaNG <chen971023@gmail.com>
2025-12-31 22:10:54 +0800

    click

--- README.md
+++
@@ -1,5 +0,0 @@
-$main.js -> 畫面物件$
-
-$index.html -> 介面$
-
-$style.css -> UI$
--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 const body = document.body;
 const app = document.createElement("div");
 
-
 app.innerHTML = `
-<div>
+<div class="chat">
     <h1>聊天室</h1>
     <div>
       <!-- 這裡之後放訊息 -->
     </div>
     <input type="text">
-    <button>送出</button>
-  </div>
+    <button class="button">送出</button>
+</div>
 `
 
+const button = app.querySelector("button");
 body.appendChild(app);
+
+button.addEventListener("click", () =>
+{
+  console.log("clicked");
+});
