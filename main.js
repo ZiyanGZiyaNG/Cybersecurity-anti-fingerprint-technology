7ea5a1ce0686f9aafaf6e2282e356c4ec01bae62
ZiaynGZiyaNG <chen971023@gmail.com>
2026-01-01 02:12:37 +0800

    list

--- main.js
+++ main.js
@@ -4,19 +4,25 @@ const app = document.createElement("div");
 app.innerHTML = `
 <div class="chat">
     <h1>聊天室</h1>
-    <div>
-
-    </div>
+    <div class="message"></div>
     <input type="text">
     <button class="button">送出</button>
 </div>
 `
 
+body.appendChild(app);
+
+
 const button = app.querySelector("button");
 const input = app.querySelector("input")
-body.appendChild(app);
+const message = app.querySelector(".message")
 
 button.addEventListener("click", () =>
 {
-  console.log(input.value)
+  const msg = document.createElement("p");
+  msg.innerText = input.value;
+  message.appendChild(msg);
+
+  input.value = "";
 });
+
